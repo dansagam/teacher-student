18dc31e4100914eeadce41417ace5d5f7f40bcd2
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2023-09-20 23:53:14 +0100

    feat: add the teacher drawer and the mmodified the student drawer

--- src/components/teacher/AddTeacherDrawer.tsx
+++ src/components/teacher/AddTeacherDrawer.tsx
@@ -1,18 +1,18 @@
 import Drawer from "@/shared/Drawer/Drawer";
 import React from "react";
 import { UseFormReturn } from "react-hook-form";
-import { StudentPayloadType } from "./validator";
 import ControlledInput from "@/shared/Input/ControlledInput";
+import { TeacherPayloadType } from "./validator";
 
-type AddStudentDrawerProps = Pick<
+type AddTeacherDrawerProps = Pick<
   React.ComponentProps<typeof Drawer>,
-  "actionText" | "open" | "onClose" | "loading"
+  "open" | "onClose" | "loading"
 > & {
   isEdit?: boolean;
-  form: UseFormReturn<StudentPayloadType>;
-  onAction(_values: StudentPayloadType): void;
+  form: UseFormReturn<TeacherPayloadType>;
+  onAction(_values: TeacherPayloadType): void;
 };
-function AddStudentDrawer(props: AddStudentDrawerProps) {
+function AddTeacherDrawer(props: AddTeacherDrawerProps) {
   const { open, onClose, isEdit, form, onAction } = props;
   const { control, handleSubmit } = form;
 
@@ -20,20 +20,22 @@ function AddStudentDrawer(props: AddStudentDrawerProps) {
     <Drawer
       open={open}
       onClose={onClose}
-      title={isEdit ? "Modify Student" : "Add Student"}
+      title={isEdit ? "Modify Teacher" : "Add Teacher"}
       isFooter
       onAction={handleSubmit(onAction)}
       actionText={isEdit ? "Edit" : "Add"}
     >
       <div className=" flex flex-col gap-5 ">
+        <ControlledInput control={control} name="title" label="Title" />
         <ControlledInput control={control} name="firstname" label="First Name" />
         <ControlledInput control={control} name="surname" label="Surname" />
         <ControlledInput control={control} name="date" type="date" label="Date of Birth" />
         <ControlledInput control={control} name="nationId" label="National ID" />
-        <ControlledInput control={control} name="studentNo" label="Teacher Number" />
+        <ControlledInput control={control} name="teacherNo" label="Teacher Number" />
+        <ControlledInput control={control} name="salary" isNumber label="Salary" />
       </div>
     </Drawer>
   );
 }
 
-export default AddStudentDrawer;
+export default AddTeacherDrawer;
---
+++ src/components/teacher/validator.ts
@@ -0,0 +1,28 @@
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { subYears } from "date-fns";
+
+const teacherSchema = yup.object({
+  title: yup.string().oneOf(["Mr", "Mrs", "Miss", "Dr", "Prof"]).required("Title is required"),
+  firstname: yup.string().required("Firstname is required"),
+  surname: yup.string().required("Surname is required"),
+  date: yup
+    .date()
+    .required("Date of Birth is required")
+    .max(subYears(new Date(), 21), "Age may not be less than 21"),
+  teacherNo: yup.string().required("Teacher Number is required"),
+  salary: yup.number().optional(),
+  nationId: yup.string().required("National Number is required"),
+});
+
+export interface TeacherPayloadType extends yup.InferType<typeof teacherSchema> {}
+export const teacherDefaultValues: TeacherPayloadType = {
+  title: "Mr",
+  firstname: "",
+  surname: "",
+  date: new Date(),
+  teacherNo: "",
+  nationId: "",
+  salary: 0,
+};
+export const teacherResolver = yupResolver(teacherSchema);
